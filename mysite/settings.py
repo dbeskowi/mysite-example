74af79e043d6f5e9c6f3ec4e46404a8eb09de88f
dbeskowi <dbeskowiney@cogeco.ca>
2021-10-04 08:22:05 -0400

    Changed mysite.settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1h+ow#x8nz2y^%xt^walxe(gt!@=hc04!lebbcvi7_5!7lozs=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
